3241c79d5fd297577160f79d941fc1fcce402410
badfeanor <badfeanor@yandex.ru>
2022-10-31 17:10:15 +0300

    reconfigure all buttons and functions

--- src/settings.py
+++ src/settings.py
@@ -3,6 +3,7 @@ from pydantic import BaseSettings
 class Settings(BaseSettings):
     BOT_TOKEN: str
     BOT_NAME: str
+    YANDEX_TOKEN: str
     LOG_FILE_PATH: str = "log_file"
     class Config:
         env_file = '../.env'
