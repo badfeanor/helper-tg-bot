c8578974a872a44b0e7bcfa9e8c279b60a626c6a
badfeanor <badfeanor@yandex.ru>
2022-11-17 09:24:19 +0300

    changes for test notificator

--- src/scheduler.py
+++ src/scheduler.py
@@ -2,5 +2,5 @@ import telebot
 from settings import config
 from bot_functions import yandex_weather_new
 
-bot = telebot.TeleBot(config.BOT_NOTIFICATOR_TOKEN)
+bot = telebot.TeleBot(config.BOT_NOTIFICATOR_TOKEN, parse_mode="markdown")
 bot.send_message(config.CHANNEL_CHAT_ID, yandex_weather_new())
